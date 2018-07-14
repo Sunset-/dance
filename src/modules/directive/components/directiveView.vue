233c1ea45b4247768585a11f0524d52f8701eb69
liudan <liudan_xa@netposa.com>
2018-07-14 15:50:14 +0800

    【指令】

--- src/modules/directive/components/directiveView.vue
+++ src/modules/directive/components/directiveView.vue
@@ -2,8 +2,34 @@
 	<div style="transform:translate(0,0)">
 		<xui-modal ref="modal" :options="options" @opened="tip('opened')" @closed="tip('closed')">
 			<template slot-scope="props" slot="content">
-				<div v-if="props.opened" style="width:500px;height:300px;">
-                    <xui-form ref="form"  :options="fieldOptions" @submit="submitHandle"></xui-form>
+				<div v-if="props.opened" style="width:500px;height:330px;">
+					<div class="directive-form">
+						<div class="form-item" v-if="type=='edit'"><div class="user-lable">指令ID</div><div class="user-operate"><span>{{modelDirective.id}}</span></div></div>
+						<div class="form-item"><div class="user-lable">指令类型</div>
+							<div class="user-operate">
+								<xui-select v-model="modelDirective.type" :options="typeOptions"></xui-select>
+							</div>
+						</div>
+						<div class="form-item"><div class="user-lable">指令匹配</div>
+							<div class="user-operate">
+								<xui-select v-model="modelDirective.match1" :options="matchOptions1"></xui-select>
+								<xui-select v-model="modelDirective.match2" :options="matchOptions2"></xui-select>
+								<xui-select v-model="modelDirective.match3" :options="matchOptions3"></xui-select>
+								<xui-select v-model="modelDirective.match4" :options="matchOptions4"></xui-select>
+							</div>
+						</div>
+						<div class="form-item"><div class="user-lable">指令名称</div>
+							<div class="user-operate">
+							   <xui-input v-model="modelDirective.name"  :options="inputOnptions"></xui-input>
+							</div>
+						</div>
+						<div class="form-item"><div class="user-lable">触发提示</div><div class="user-operate">
+						 <xui-input v-model="modelDirective.tips" :options="inputOnptions"></xui-input>
+						</div></div>
+						<div class="form-item"><div class="user-lable">触发词</div><div class="user-operate">
+						 <xui-input v-model="modelDirective.tips" :options="textareaOnptions"></xui-input></div>
+						 </div>
+					</div>
 				</div>
 			</template>
 			<div slot="footer" style="padding:10px;text-align:center;">
@@ -14,38 +40,118 @@
 </template>
 
 <script>
-import FORMFIELD from "../options/directFormField.js"
 export default {
+	props:['type'],
 	methods: {
 		open: function() {
 			this.$refs.modal.open();
 		},
 		tip: function(msg) {
 			console.log(msg);
+		},
+		submitHandle(){
+			
 		}
 	},
 	data() {
 		return {
-            fieldOptions:{
-                cols: 2,
-				fields: [].concat(FORMFIELD)
+			
+			modelDirective:{
+				type:"0",
+				match1:"0",
+				match2:"0",
+				match3:"0",
+				match4:"0"
+			},
+			matchOptions1:{
+				 multiple: false,
+                clearable: true,
+                filter: false,
+				style:"width:80px;",
+                data: [
+                   {
+                        text: "舞蹈",
+                        value: "0"
+                    }
+                ]
+			},
+			matchOptions2:{
+				 multiple: false,
+                clearable: true,
+                filter: false,
+				style:"width:80px;",
+                data: [
+                   {
+                        text: "舞步",
+                        value: "0"
+                    }
+                ]
+			},
+			matchOptions3:{
+				 multiple: false,
+                clearable: true,
+                filter: false,
+				style:"width:80px;",
+                data: [
+                   {
+                        text: "课程",
+                        value: "0"
+                    }
+                ]
+			},
+			matchOptions4:{
+				 multiple: false,
+                clearable: true,
+                filter: false,
+				style:"width:80px;",
+                data: [
+                   {
+                        text: "模块",
+                        value: "0"
+                    }
+                ]
+			},
+			 typeOptions: {
+                multiple: false,
+                clearable: true,
+                filter: false,
+                data: [
+                   {
+                        text: "控制指令",
+                        value: "0"
+                    },
+					{
+                        text: "选择指令",
+                        value: "1"
+                    }
+                ]
             },
 			options: {
 				top:200,
 				title: "",
 				maskClose: true
 			},
+			inputOnptions:{
+				 type: "text",
+				  clearable: true,
+			},
+			textareaOnptions:{
+				style:"width: 220px;",
+				 type: "textarea",
+				clearable: true,
+			},
 			toolbarOptions: {
 				size: "medium",
 				tools: [
 					{
 						label: "保存",
-						color: "success",
+						style:"background:#4081FF;color:#fff;border-radius:20px;margin-right:20px;padding:15px 30px;line-height:0px",
+						// color: "success",
 						operate() {}
 					},
 					{
 						label: "取消",
-						color: "info",
+						style:"background:#CCCCCC;color:#fff;border-radius:20px;padding:15px 30px;line-height:0px",
 						operate: () => {
 							this.$refs.modal.close();
 						}
@@ -56,3 +162,24 @@ export default {
 	}
 };
 </script>
+<style lang="less">
+	.directive-form{
+		overflow:hidden;
+		padding:20px 30px;
+		.form-item{
+			display:block;
+			    margin: 20px 0px;
+    			overflow: hidden;
+    			min-height: 30px;
+		}
+		.form-item div{
+			display:inline-block;
+			float:left;
+		}
+		.form-item .user-lable{
+			width:90px;
+			text-align:center;
+			line-height: 30px;
+		}
+	}
+</style>
--- src/modules/directive/index.vue
+++ src/modules/directive/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <xui-table :options="directiveOptionsTable" class="xui-style-table" ref="dTableRef">
-            <noresult class="org-no-result" slot="empty" :isShow="true" :mainText="'该组织下尚未添加下级组织机构'" :subText="'点击新增组织机构&gt;&gt;'" size="large" @subTextFun="openModal()"></noresult>
+           
         </xui-table>
         <div class="add-directive">
-              <xui-button @click="openModal" color="white" icon="el-icon-info" label="123">添加指令</xui-button>
+              <xui-button @click="openModal" color="white" icon="el-icon-add" label="123" class="directive-addbtn">添加指令</xui-button>
         </div>
-        <directive-view ref="moduleRef"></directive-view>
+        <directive-view ref="moduleRef" :type="currentType"></directive-view>
     </div>
 </template>
 <script>
@@ -15,26 +15,29 @@ import directiveView from "./components/directiveView"
 export default {
 	data() {
 		return {
+			currentType:"add",
 			directiveOptionsTable: {
                 flag:true,
 				columns: DIRECTIVECOLUMN.concat([
 					{
-						title: "操作",
+						title: "",
 						style: "text-align:center;width:180px",
 						toolbar: {
 							tools: [
 								{
 									label: "编辑",
+									style:"opacity:0;background:#2F7CEF;color:#fff;border-radius:10px",
 									// icon: "icon-table-edit icon-base-org icon-table-common", //编辑
 									// premise: () => {
 									// 	return this.flag;
 									// },
 									operate: record => {
-                                        this.open();
+                                        this.openModal();
 										// this.orgEventRecive("edit", record.id);
 									}
 								},
 								{
+									style:"opacity:0;background:#FC9167;color:#fff;border-radius:10px",
 									label: "删除",
 									align: "center",
 									// icon: "icon-table-delete icon-table-common", //删除
@@ -100,9 +103,34 @@ export default {
 	mounted() {}
 };
 </script>
-<style lang="less" scoped>
+<style lang="less">
+.xui-style-table .xui-datatable-table tr td,.xui-style-table .xui-datatable-table tr th{
+	border:0px solid #000;
+}
+.xui-style-table .xui-datatable-table tr:hover button{
+	opacity:1!important;
+}
+.el-icon-add{
+	background:url("/assets/directive/icon/icon-add.png");
+	width:16px;
+	height:16px;
+	background-repeat:no-repeat;
+	vertical-align: middle;
+}
 .add-directive{
     text-align: center;
     margin-top: 30px;
 }
+.hidden{
+	opacity:0;
+}
+.directive-addbtn{
+	background:#4081FF;
+	color:#fff;
+	width:160px;
+	height:30px;
+	border-radius:10px;
+	text-align:center;
+}
+
 </style>
