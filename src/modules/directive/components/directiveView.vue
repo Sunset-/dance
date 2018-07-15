9de2ecaa9ab88a0b13d7c3c2f6490608605d6020
liudan <liudan_xa@netposa.com>
2018-07-15 11:41:38 +0800

    ss

--- src/modules/directive/components/directiveView.vue
+++ src/modules/directive/components/directiveView.vue
@@ -2,7 +2,7 @@
 	<div style="transform:translate(0,0)">
 		<xui-modal ref="modal" :options="options" @opened="tip('opened')" @closed="tip('closed')">
 			<template slot-scope="props" slot="content">
-				<div v-if="props.opened" style="width:500px;min-height: 330px;">
+				<div v-if="props.opened" style="width:560px;min-height: 330px;">
 					<div class="directive-form">
 						<div class="form-item" v-if="type=='edit'">
 							<div class="user-lable">指令ID</div>
@@ -26,22 +26,24 @@
 						</div>
 						<div class="form-item">
 							<div class="user-lable">指令名称</div>
-							<div class="user-operate">
+							<div class="user-operate" :class="{'error':modelDirectiveError.error}">
 								<xui-input v-model="modelDirective.name" :options="inputOnptions"></xui-input>
+								<p class="error-tips" v-show="modelDirectiveError.error">{{modelDirectiveError.tips}}</p>
 							</div>
 						</div>
 						<div class="form-item">
 							<div class="user-lable">触发提示</div>
 							<div class="user-operate">
 								<xui-input v-model="modelDirective.tips" :options="inputOnptions" style="width:217px;"></xui-input>
-								<div class="parameter-seting-btn">参数设置</div>
-								<div class="parameter-list">
+								<div class="parameter-seting-btn" @click="showParams=!showParams">参数设置</div>
+								<div class="parameter-list" v-show="showParams" >
 									<div v-for="(item,index) in parameterData" :key="index">
 										<span>{{item.name}}</span>
-										<xui-input v-model="item.item1.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item1.placeholder"></xui-input>
-										<xui-input v-model="item.item2.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item1.placeholder"></xui-input>
-										<xui-input v-model="item.item3.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item1.placeholder" v-if="item.name!='标题'"></xui-input>
-										<xui-input v-model="item.item4.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item1.placeholder" v-if="item.name!='标题'"></xui-input>
+										<xui-input v-model="item.item1.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item1.placeholder" :maxlength="item.item1.maxLength"></xui-input>
+										<span v-if="item.name=='标题'" style="margin-left:26px">{{item.name2}}</span>
+										<xui-input v-model="item.item2.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item2.placeholder" :class="{'direct-class':item.name=='标题'}"></xui-input>
+										<xui-input v-model="item.item3.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item3.placeholder" v-if="item.name!='标题'"></xui-input>
+										<xui-input v-model="item.item4.value" :options="inputOnptions" class="parameter-input" :placeholder="item.item4.placeholder" v-if="item.name!='标题'"></xui-input>
 									</div>
 								</div>
 							</div>
@@ -50,14 +52,16 @@
 							<div class="user-lable">触发词</div>
 							<div class="user-operate">
 								<i class="icon-add-item" @click="addList"></i>
-								<div class="triggle-list">
+								<div class="triggle-list" :class="{'error-parameter':modelDirectiveContextError.error}">
 									<xui-scroll style="height:100%;">
 										<p v-for="(item,index)  in triggleData" :key="index">
-											<xui-input v-model="item.name" :options="inputOnptions" class="triggle-input"></xui-input>
+											<xui-input v-model="item.name" :options="inputOnptions" class="triggle-input" @change="triggleTest"></xui-input>
 											<i class="icon-delete-item" @click="deletList(index)"></i>
 										</p>
 									</xui-scroll>
+
 								</div>
+								<p class="error-tips" v-show="modelDirectiveContextError.error">{{modelDirectiveContextError.tips}}</p>
 								<!-- <xui-input v-model="modelDirective.tips" :options="textareaOnptions"></xui-input> -->
 							</div>
 						</div>
@@ -72,6 +76,39 @@
 </template>
 
 <script>
+function specilWorldTest(a) {
+	if (a == "") {
+		return false;
+	}
+	var regEn = /[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im,
+		regCn = /[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im;
+
+	if (regEn.test(a) || regCn.test(a)) {
+		return true;
+	}
+	return false;
+}
+function number(a) {
+	var pattern = new RegExp("^[0-9]*$");
+	if (pattern.test(a) && a <= 100 && a >= 0) {
+		return true;
+	}
+	return false;
+}
+function timeOffset(a) {
+	var pattern = new RegExp("^[0-9]*$"),
+		pattern2 = new RegExp("^[-|0-9][0-9]* ");
+	if ((pattern.test(a) || pattern2.test(a)) && a <= 100) {
+		return true;
+	}
+	return false;
+}
+function number(a) {
+	if (a <= 100 && a >= 0) {
+		return true;
+	}
+	return false;
+}
 export default {
 	props: ["type"],
 	methods: {
@@ -82,62 +119,161 @@ export default {
 			console.log(msg);
 		},
 		addList() {
+			if(this.triggleData.length>0){
+				var cc = this.triggleData.filter(item=>{
+					return item.name!="";
+				})
+				if(cc.length==0){
+
+				}
+			}
 			var c = { name: "" };
 			this.triggleData.push(c);
 		},
 		deletList(index) {
 			this.triggleData.splice(index, 1);
+		},
+		//触发词校验
+		triggleTest(val) {
+			if (val) {
+				if (specilWorldTest(val)) {
+					this.modelDirectiveContextError.error = true;
+				} else {
+					this.modelDirectiveContextError.error = false;
+				}
+			} else {
+				this.modelDirectiveContextError.error = false;
+			}
+		},
+		reset(){
+			this.triggleData=[];
+			this.modelDirective={
+				name: "",
+				type: "0",
+				match1: "0",
+				match2: "0",
+				match3: "0"
+			};
+			this.parameterData=[
+				{
+					name: "动作",
+					item1: { value: "", placeholder: "初级示范" },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
+				},
+				{
+					name: "表情",
+					item1: { value: "", placeholder: "名称",maxLength:10 },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
+				},
+				{
+					name: "镜头",
+					item1: { value: "", placeholder: "名称",maxLength:10 },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
+				},
+				{
+					name: "动效",
+					item1: { value: "", placeholder: "名称",maxLength:10 },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
+				},
+				{
+					name: "标题",
+					item1: { value: "", placeholder: "向右走步特",maxLength:6 },
+					name2: "任务方向",
+					item2: { value: "", placeholder: "0/180" }
+				}
+			];
+		}
+	},
+	watch: {
+		"modelDirective.name": function(val) {
+			if (val) {
+				if (specilWorldTest(val)) {
+					this.modelDirectiveError.error = true;
+				} else {
+					this.modelDirectiveError.error = false;
+				}
+			} else {
+				this.modelDirectiveError.error = false;
+			}
+		},
+		'type':function(val){
+			if(val=='add'){
+
+			}
 		}
 	},
 	data() {
 		return {
 			triggleData: [],
+			showParams: false,
+			modelDirectiveError:{
+				error:false,
+				tips:"指令出现特殊字符"
+			},
+			modelDirectiveContextError:{
+				error:false,
+				tips:"指令出现特殊字符"
+			},
+			modelDirectiveTipsError:{
+				error:false,
+				tips:"指令出现特殊字符"
+			},
 			modelDirective: {
+				name: "",
 				type: "0",
 				match1: "0",
 				match2: "0",
-				match3: "0",
+				match3: "0"
 			},
 			parameterData: [
 				{
 					name: "动作",
-					item1: { value: "", placeholder: "" },
-					item2: { value: "", placeholder: "" },
-					item3: { value: "", placeholder: "" },
-					item4: { value: "", placeholder: "" }
+					item1: { value: "", placeholder: "初级示范" },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
 				},
 				{
 					name: "表情",
-					item1: { value: "", placeholder: "" },
-					item2: { value: "", placeholder: "" },
-					item3: { value: "", placeholder: "" },
-					item4: { value: "", placeholder: "" }
+					item1: { value: "", placeholder: "名称",maxLength:10 },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
 				},
 				{
 					name: "镜头",
-					item1: { value: "", placeholder: "" },
-					item2: { value: "", placeholder: "" },
-					item3: { value: "", placeholder: "" },
-					item4: { value: "", placeholder: "" }
+					item1: { value: "", placeholder: "名称",maxLength:10 },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
 				},
 				{
 					name: "动效",
-					item1: { value: "", placeholder: "" },
-					item2: { value: "", placeholder: "" },
-					item3: { value: "", placeholder: "" },
-					item4: { value: "", placeholder: "" }
+					item1: { value: "", placeholder: "名称",maxLength:10 },
+					item2: { value: "", placeholder: "ID" },
+					item3: { value: "", placeholder: "触发时机" },
+					item4: { value: "", placeholder: "偏移时间" }
 				},
 				{
 					name: "标题",
-					item1: { value: "", placeholder: "" },
-					item2: { value: "", placeholder: "" }
+					item1: { value: "", placeholder: "向右走步特",maxLength:6 },
+					name2: "任务方向",
+					item2: { value: "", placeholder: "0/180" }
 				}
 			],
 			matchOptions1: {
 				multiple: false,
 				clearable: true,
 				filter: false,
-				style: "width:80px;",
+				style: "width:90px;",
 				data: [
 					{
 						text: "等级",
@@ -149,7 +285,7 @@ export default {
 				multiple: false,
 				clearable: true,
 				filter: false,
-				style: "width:80px;",
+				style: "width:90px;",
 				data: [
 					{
 						text: "课程",
@@ -161,7 +297,7 @@ export default {
 				multiple: false,
 				clearable: true,
 				filter: false,
-				style: "width:80px;",
+				style: "width:90px;",
 				data: [
 					{
 						text: "模块",
@@ -185,17 +321,12 @@ export default {
 				]
 			},
 			options: {
-				top: 200,
+				top: 100,
 				title: "",
 				maskClose: true
 			},
 			inputOnptions: {
 				type: "text",
-				clearable: true
-			},
-			textareaOnptions: {
-				style: "width: 220px;",
-				type: "textarea",
 				clearable: true
 			},
 			toolbarOptions: {
@@ -222,13 +353,18 @@ export default {
 };
 </script>
 <style lang="less">
+.error .xui-input-style .el-input--suffix .el-input__inner,
+.directive-form .error-parameter.triggle-list:hover,
+.directive-form .error-parameter.triggle-list{
+	border: 1px solid #e22929;
+}
+
 .directive-form {
 	overflow: hidden;
 	padding: 20px 30px;
 	.form-item {
 		display: block;
 		margin: 20px 0px;
-		overflow: hidden;
 		min-height: 30px;
 	}
 	.form-item > div {
@@ -241,12 +377,16 @@ export default {
 		text-align: center;
 		line-height: 30px;
 	}
-	.parameter-list
-	{
-		left: -38px;
-    	position: relative;
+	.parameter-list {
+		left: -50px;
+		position: relative;
+		text-align: left;
+		margin: 20px 0px 10px 0px;
+		padding-left: 15px;
+		border-left: 1px solid #ccc;
 	}
 	.parameter-seting-btn {
+		cursor: pointer;
 		width: 84px;
 		height: 30px;
 		line-height: 30px;
@@ -262,10 +402,13 @@ export default {
 	}
 	.parameter-input {
 		display: inline-block;
-		width: 60px;
+		width: 70px;
 		border-radius: 0px;
 		height: 30px;
 		margin: 5px;
+	}
+	.parameter-input.direct-class {
+		width: 152px;
 	}
 	.triggle-list {
 		width: 220px;
@@ -317,6 +460,15 @@ export default {
 			top: 2px;
 			left: 2px;
 		}
+	}
+	.error-tips {
+		color: #e22929;
+		font-size: 12px;
+		text-align: left;
+		/* margin-top: 10px; */
+		position: absolute;
+		left: 10px;
+		bottom: -14px;
 	}
 }
 </style>
--- src/modules/directive/index.vue
+++ src/modules/directive/index.vue
@@ -1,25 +1,25 @@
 <template>
-    <div>
-        <xui-table :options="directiveOptionsTable" class="xui-style-table" ref="dTableRef">
-           
-        </xui-table>
-        <div class="add-directive">
-              <xui-button @click="openModal" color="white" icon="el-icon-add" label="123" class="directive-addbtn">添加指令</xui-button>
-        </div>
-        <directive-view ref="moduleRef" :type="currentType"></directive-view>
+	<div>
+		<xui-table :options="directiveOptionsTable" class="xui-style-table" ref="dTableRef">
+
+		</xui-table>
+		<div class="add-directive">
+			<xui-button @click="openModal" color="white" icon="el-icon-add" label="123" class="directive-addbtn">添加指令</xui-button>
+		</div>
+		<directive-view ref="moduleRef" :type="currentType"></directive-view>
 		<del ref="delRef"></del>
-    </div>
+	</div>
 </template>
 <script>
 import DIRECTIVECOLUMN from "./options/tableColumn.js";
 import directiveView from "./components/directiveView";
-import del from "./components/del.vue"
+import del from "./components/del.vue";
 export default {
 	data() {
 		return {
-			currentType:"add",
+			currentType: "add",
 			directiveOptionsTable: {
-                flag:true,
+				flag: true,
 				columns: DIRECTIVECOLUMN.concat([
 					{
 						title: "",
@@ -28,18 +28,18 @@ export default {
 							tools: [
 								{
 									label: "编辑",
-									style:"opacity:0;background:#2F7CEF;color:#fff;border-radius:10px",
+									style: "opacity:0;background:#2F7CEF;color:#fff;border-radius:10px",
 									// icon: "icon-table-edit icon-base-org icon-table-common", //编辑
 									// premise: () => {
 									// 	return this.flag;
 									// },
 									operate: record => {
-                                        this.openModal();
+										this.openModal();
 										// this.orgEventRecive("edit", record.id);
 									}
 								},
 								{
-									style:"opacity:0;background:#FC9167;color:#fff;border-radius:10px",
+									style: "opacity:0;background:#FC9167;color:#fff;border-radius:10px",
 									label: "删除",
 									align: "center",
 									// icon: "icon-table-delete icon-table-common", //删除
@@ -47,6 +47,7 @@ export default {
 									// 	return this.flag;
 									// },
 									operate: record => {
+										this.$refs.delRef.open();
 										// this.deleteOrgEvent(record);
 									}
 								}
@@ -61,31 +62,31 @@ export default {
 					count: "length",
 					currentPage: "currentPage",
 					pageSize: "pageSize"
-                },
-                datasource: filter => {
-                    var data=[
-                        {
-                            type:"选择指令",
-                            name:"暂停",
-                            trigger:"灵儿停下来等你奥",
-                            triggerWord:"暂停、等等、等下"
-                        },
-                        {
-                            type:"选择指令",
-                            name:"暂停",
-                            trigger:"灵儿停下来等你奥",
-                            triggerWord:"暂停、等等、等下"
-                        },
-                        {
-                            type:"选择指令",
-                            name:"暂停",
-                            trigger:"灵儿停下来等你奥",
-                            triggerWord:"暂停、等等、等下"
-                        }
-                    ]
-                    return {
-                        data:data
-                    }
+				},
+				datasource: filter => {
+					var data = [
+						{
+							type: "选择指令",
+							name: "暂停",
+							trigger: "灵儿停下来等你奥",
+							triggerWord: "暂停、等等、等下"
+						},
+						{
+							type: "选择指令",
+							name: "暂停",
+							trigger: "灵儿停下来等你奥",
+							triggerWord: "暂停、等等、等下"
+						},
+						{
+							type: "选择指令",
+							name: "暂停",
+							trigger: "灵儿停下来等你奥",
+							triggerWord: "暂停、等等、等下"
+						}
+					];
+					return {
+						data: data
+					};
 					// return RoleStore.loadRolesByType(this.currentType).then(res => {
 					// 	this.roleCount = res.list.length;
 					// 	return res;
@@ -93,47 +94,48 @@ export default {
 				}
 			}
 		};
-    },
-    components:{
-        directiveView,
+	},
+	components: {
+		directiveView,
 		del
-    },
-	methods:{
-        openModal(){
-            this.$refs.moduleRef.open();
-        }
-    },
+	},
+	methods: {
+		openModal() {
+			this.$refs.moduleRef.open();
+		}
+	},
 	mounted() {}
 };
 </script>
 <style lang="less">
-.xui-style-table .xui-datatable-table tr td,.xui-style-table .xui-datatable-table tr th{
-	border:0px solid #000;
+.xui-style-table .xui-datatable-table tr td,
+.xui-style-table .xui-datatable-table tr th {
+	border: 0px solid #000;
 }
-.xui-style-table .xui-datatable-table tr:hover button{
-	opacity:1!important;
+.xui-style-table .xui-datatable-table tr:hover button {
+	opacity: 1 !important;
 }
-.el-icon-add{
-	background:url("/assets/directive/icon/icon-add.png");
-	width:16px;
-	height:16px;
-	background-repeat:no-repeat;
+.el-icon-add {
+	background: url("/assets/directive/icon/icon-add.png");
+	width: 16px;
+	height: 16px;
+	background-repeat: no-repeat;
 	vertical-align: middle;
 }
-.add-directive{
-    text-align: center;
-    margin-top: 30px;
+.add-directive {
+	text-align: center;
+	margin-top: 30px;
 }
-.hidden{
-	opacity:0;
+.hidden {
+	opacity: 0;
 }
-.directive-addbtn{
-	background:#4081FF;
-	color:#fff;
-	width:160px;
-	height:30px;
-	border-radius:10px;
-	text-align:center;
+.add-directive .directive-addbtn {
+	background: #4081ff;
+	color: #fff;
+	width: 160px;
+	height: 36px;
+	border-radius: 20px;
+	text-align: center;
+	font-size: 14px;
 }
-
 </style>
