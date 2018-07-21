366fb46e59b729ee17d420e2052e7da8b1c9bc33
fanliangliang <fanliangliang@netposa.com>
2018-07-21 13:07:52 +0800

    u

--- src/main.js
+++ src/main.js
@@ -19,6 +19,9 @@ Vue.use(XUI);
 //引入公共服务
 import "./services/index";
 
+//引入公共组件
+import "./components/index";
+
 //开始
 import App from "./App.vue";
 import Modules from "./Modules.vue";
@@ -27,39 +30,41 @@ import Course from "modules/course/index.vue";
 import Directive from "modules/directive/index.vue";
 
 var router = new Router({
-    routes: [{
-            path: "/",
-            component: Modules,
-            redirect: "/course",
-            children: [{
-                    name: "course",
-                    path: "/course",
-                    component: Course
-                },
-                {
-                    name: "directive",
-                    path: "/directive",
-                    component: Directive
-                }
-            ]
-        },
-        {
-            path: "/login",
-            component: Login
-        }
-    ]
+	routes: [
+		{
+			path: "/",
+			component: Modules,
+			redirect: "/course",
+			children: [
+				{
+					name: "course",
+					path: "/course",
+					component: Course
+				},
+				{
+					name: "directive",
+					path: "/directive",
+					component: Directive
+				}
+			]
+		},
+		{
+			path: "/login",
+			component: Login
+		}
+	]
 });
 window.$router = router;
 //判断用户是否已登录
-router.beforeEach((to, from, next)=>{
-    if(to.path == '/login'){
-        next()
-    }else{
-    	if(!window.sessionStorage.getItem("user")){
-            next({path: '/login'})
-		}else {
-            next();
+router.beforeEach((to, from, next) => {
+	if (to.path == "/login") {
+		next();
+	} else {
+		if (!window.sessionStorage.getItem("user")) {
+			next({ path: "/login" });
+		} else {
+			next();
 		}
-    }
+	}
 });
 new Vue({ el: "#app", router, render: h => h(App) });
