4ffe860a3f9a122d3571979cf295d17adb0b8885
chaizhixiang@netposa.com <chaizhixiang@netposa.com>
2018-07-21 16:34:20 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ import "./services/index";
 import App from "./App.vue";
 import Modules from "./Modules.vue";
 import Login from "modules/login/index.vue";
+import Reset from "modules/login/reset.vue";
 import Course from "modules/course/index.vue";
 import Directive from "modules/directive/index.vue";
 
@@ -46,13 +47,34 @@ var router = new Router({
         {
             path: "/login",
             component: Login
+        },
+        {
+            path: "/reset",
+            component: Reset
         }
     ]
 });
 window.$router = router;
 //判断用户是否已登录
 router.beforeEach((to, from, next)=>{
-    if(to.path == '/login'){
+    if(to.query && to.query.verify_code){
+        //判断验证链接是否有效，有效则调到修改密码页，无效跳转到登录页。
+        $http({
+            url: "dance/reset_pwd/"+to.query.verify_code,
+            type: "GET",
+        }).then(res=>{
+           if(res&&res.msg=='Ok'){
+               window.localStorage.setItem("verify_code",to.query.verify_code);
+               next({path: '/reset'});
+           }
+           if(res&&res.msg=='This account not exist.'){
+               $tip(`密码验证链接已失效，请重新点击忘记密码获取密码验证链接`,"warning");
+               window.localStorage.removeItem("verify_code");
+               next({path: '/login'});
+           }
+        });
+    }
+    if(to.path == '/login' || to.path == '/reset'&&window.localStorage.getItem("verify_code")){
         next()
     }else{
     	if(!window.sessionStorage.getItem("user")){
