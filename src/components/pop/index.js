cc1ade611876d5e5e6b7138795d068de78b921de
fanliangliang <fanliangliang@netposa.com>
2018-07-22 21:15:31 +0800

    pop bug

--- src/components/pop/index.js
+++ src/components/pop/index.js
@@ -3,14 +3,15 @@ import "./pop.scss";
 var timer = null;
 
 $("body").on("mouseenter", ".xui-pop", function(ev) {
-    var $target = $(ev.target),
-        $this = $(this),
+	clearTimeout(timer);
+	var $target = $(ev.target),
+		$this = $(this),
 		popData = $this.attr("data-content") || "POP";
 	var $pop = $(".xui-pop-wrap");
 	if (!$pop.length) {
 		$pop = $('<div class="xui-pop-wrap"><div class="xui-pop-wrap-inner">' + popData + "</div></div>");
 	} else {
-		$('.xui-pop-wrap-inner').html(popData);
+		$(".xui-pop-wrap-inner").html(popData);
 	}
 	$pop.appendTo($("body")).show();
 	var offset = $this.offset();
